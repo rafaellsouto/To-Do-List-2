60cea9da7a84b5d4ac5f8d00ceacfcb10093a9ff
Rafael Souto <120722217+rafaellsouto@users.noreply.github.com>
2023-03-10 12:04:24 -0300

    To Do List 2

---
+++ script.js
@@ -0,0 +1,55 @@
+function addTask()
+{
+    const taskTitle = document.querySelector('#task-title').value
+
+    if( taskTitle )
+    {
+        const template = document.querySelector('.template')
+
+        const newTask = template.cloneNode(true)
+
+        newTask.querySelector('.task-title').textContent = taskTitle
+
+        newTask.classList.remove('template')
+        newTask.classList.remove('hide')
+
+        const list = document.querySelector('#task-list')
+
+        list.appendChild(newTask)
+
+        document.querySelector('#task-title').value = ''
+
+        const removeBtn = newTask.querySelector('.remove-btn').addEventListener('click', function(){
+            removeTask(this)
+        })
+
+        const doneBtn = newTask.querySelector('.done-btn').addEventListener('click', function(){
+            doneTask(this)
+        })
+
+    } else {
+        alert('Voce não inseriu  nenhuma tarefa!')
+    }
+}
+
+let removeTask = (task) => 
+{
+    task.parentNode.remove(true)
+}
+
+let doneTask = (task) =>
+{
+    const taskToComplete = task.parentNode
+    taskToComplete.classList.toggle('done')
+}
+
+const btn = document.querySelector('#add-btn')
+
+btn.addEventListener("click", function(event)
+{
+    event.preventDefault()
+
+    addTask()
+})
+
+
